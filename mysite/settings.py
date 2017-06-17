4c6edb21941538949028ec5642b2ea9e37d93e7e
Barbara <barbaraafmiranda@gmail.com>
2017-06-17 15:34:04 -0300

    allowed_hosts modificar

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '(@^5b6*cm8gjji*pv43og#ifz*9r9w2g$q=ehbjbpw8uo-m1_c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['barbaramiranda.pythonanywhere.com']
+
 
 
 # Application definition
